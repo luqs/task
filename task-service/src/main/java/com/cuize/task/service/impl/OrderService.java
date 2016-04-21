de6ea34ec36730767c2969f59499ad17fd47430a
luqs <qinsong_lu@cuize.net>
2016-04-21 13:38:57 +0800

    添加一票通功能

--- task-service/src/main/java/com/cuize/task/service/impl/OrderService.java
+++ task-service/src/main/java/com/cuize/task/service/impl/OrderService.java
@@ -4,6 +4,7 @@ import java.nio.charset.Charset;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.fluent.Request;
 import org.apache.ibatis.session.RowBounds;
 import org.slf4j.Logger;
@@ -16,7 +17,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.cuize.task.dao.order.domain.OrderDetail;
 import com.cuize.task.dao.order.domain.OrderDetailExample;
+import com.cuize.task.dao.order.domain.OrderDetailPackdtl;
+import com.cuize.task.dao.order.domain.OrderDetailPackdtlExample;
 import com.cuize.task.dao.order.mapper.OrderDetailMapper;
+import com.cuize.task.dao.order.mapper.OrderDetailPackdtlMapper;
 import com.cuize.task.dao.order.mapper.OrderMapper;
 import com.cuize.task.meta.Constant;
 import com.cuize.task.service.dto.GlobalConfig;
@@ -36,61 +40,125 @@ public class OrderService {
 
 	@Autowired
 	private GlobalConfig config;
-	
-	@Autowired 
+
+	@Autowired
 	private OrderMapper orderMapper;
-	
-	@Autowired 
+
+	@Autowired
 	private OrderDetailMapper detailMapper;
 	
 	@Autowired
+	private OrderDetailPackdtlMapper packdtlMapper;
+
+	@Autowired
 	private OrderOptlogService optlogService;
-	
-	
+
 	public List<OrderDetail> getHqPaidOrders() throws Exception {
 		OrderDetailExample detailExample = new OrderDetailExample();
 		detailExample.createCriteria()
-			.andStatEqualTo(Constant.ORDER_STATUS_PAID)
-			.andThirdOrderNoLike("S%");
-		List<OrderDetail> oLst = detailMapper.selectByExample(detailExample,new RowBounds(0, 100));
-		_LOG.info("*******getHqPaidOrders*******"+oLst);
+				.andStatEqualTo(Constant.ORDER_STATUS_PAID)
+				.andThirdOrderNoLike("S%");
+		detailExample.or().andProductPacktypeEqualTo(
+				Constant.PRODUCT_PACKTYPE_HUANQI);
+		List<OrderDetail> oLst = detailMapper.selectByExample(detailExample,
+				new RowBounds(0, 100));
+		_LOG.info("*******getHqPaidOrders*******" + oLst);
 		return oLst;
 	}
+
 	/**
 	 * 产品库存入库接口
 	 * 
 	 * @author luqingsong
 	 */
 	public void synchOrderStatus(OrderDetail orderDetail) throws Exception {
-		String hqGetOrderURL= new StringBuffer(config.getHqGetOrderUrl())
+		if (orderDetail.getProductPacktype() == Constant.PRODUCT_PACKTYPE_HUANQI) {
+			//如果order_detail对应的产品是打包产品 
+			OrderDetailPackdtlExample example = new OrderDetailPackdtlExample();
+			example.createCriteria().andOrderDetailIdEqualTo(orderDetail.getId());
+			List<OrderDetailPackdtl> oPackdtlLst = packdtlMapper.selectByExample(example);
+			for(OrderDetailPackdtl oPackdtl:oPackdtlLst){
+				if(!StringUtils.isBlank(oPackdtl.getThirdOrderNo())
+						&&Constant.THIRD_SYSTYPE_HUANQI.equals(oPackdtl.getTicketSystype())){
+					String hqGetOrderURL = new StringBuffer(config.getHqGetOrderUrl())
 								.append("?otaaccount=").append(config.getHqAccount())
 								.append("&otapassWord=").append(config.getHqPassword())
-								.append("&orderno=").append(orderDetail.getThirdOrderNo())
+								.append("&orderno=").append(oPackdtl.getThirdOrderNo())
 								.toString();
-		String resJson = Request
-				.Get(hqGetOrderURL)
-				.execute().returnContent()
-				.asString(Charset.forName("utf-8"));
-		HQGetOrderRes response= JSON.parseObject(
-				resJson,new TypeReference<HQGetOrderRes>(){});
-		_LOG.info("**********timer task :"+resJson);
-		if(response.getResult().getStatus() && response.getOrder().getOrderStatus().equals("已取票")){
-			//修改订单明细的状态
-			OrderDetailExample updateExample = new OrderDetailExample();
-			updateExample.createCriteria()
-				.andIdEqualTo(orderDetail.getId())
-				.andVersionEqualTo(orderDetail.getVersion());
-			orderDetail.setStat(Constant.ORDER_STATUS_FINISH);
-			orderDetail.setVersion(orderDetail.getVersion()+1);
-			orderDetail.setUpdateTime(new Date());
-			detailMapper.updateByExampleSelective(orderDetail, updateExample);
-			
-			optlogService.saveLog(orderDetail.getOrderId(), Constant.ORDEROPT_TYPE_UPDATE, "【定时任务】同步环企订单状态【"+response.getOrder().getOrderStatus()+"】", "TimerTask");
-			_LOG.info("**********【定时任务】同步环企订单状态【"+response.getOrder().getOrderStatus()+"】");
-		}else if(!response.getResult().getStatus()){
-			optlogService.saveLog(orderDetail.getOrderId(), Constant.ORDEROPT_TYPE_UPDATE, "【定时任务】同步环企订单状态失败【"+response.getResult().getErrormessage()+"】", "TimerTask");
-			_LOG.info("**********【定时任务】同步环企订单状态失败【"+response.getResult().getErrormessage()+"】");
+					String resJson = Request.Get(hqGetOrderURL).execute()
+							.returnContent().asString(Charset.forName("utf-8"));
+					HQGetOrderRes response = JSON.parseObject(resJson,
+							new TypeReference<HQGetOrderRes>() {
+							});
+					
+					if (response.getResult().getStatus() && response.getOrder().getOrderStatus().equals("已取票")) {
+						// 修改订单明细的状态
+						OrderDetailPackdtlExample updateExample = new OrderDetailPackdtlExample();
+						updateExample.createCriteria()
+								.andIdEqualTo(oPackdtl.getId())
+								.andVersionEqualTo(oPackdtl.getVersion());
+						oPackdtl.setStat(Constant.ORDER_STATUS_FINISH);
+						oPackdtl.setVersion(orderDetail.getVersion() + 1);
+						oPackdtl.setUpdateTime(new Date());
+						packdtlMapper.updateByExampleSelective(oPackdtl, updateExample);
+
+						optlogService.saveLog(orderDetail.getOrderId(), Constant.ORDEROPT_TYPE_UPDATE, "【定时任务】同步环企订单状态【"
+										+ response.getOrder().getOrderStatus() + "】",
+								"TimerTask");
+						_LOG.info("**********【定时任务】同步环企订单状态【"
+								+ response.getOrder().getOrderStatus() + "】");
+					} else if (!response.getResult().getStatus()) {
+						optlogService.saveLog(orderDetail.getOrderId(),
+								Constant.ORDEROPT_TYPE_UPDATE, "【定时任务】同步环企订单状态失败【"
+										+ response.getResult().getErrormessage() + "】",
+								"TimerTask");
+						_LOG.info("**********【定时任务】同步环企订单状态失败【"
+								+ response.getResult().getErrormessage() + "】");
+					}
+				}
+				
+				
+			}
+
+		} else {
+			//非打包产品
+			String hqGetOrderURL = new StringBuffer(config.getHqGetOrderUrl())
+					.append("?otaaccount=").append(config.getHqAccount())
+					.append("&otapassWord=").append(config.getHqPassword())
+					.append("&orderno=").append(orderDetail.getThirdOrderNo())
+					.toString();
+			String resJson = Request.Get(hqGetOrderURL).execute()
+					.returnContent().asString(Charset.forName("utf-8"));
+			HQGetOrderRes response = JSON.parseObject(resJson,
+					new TypeReference<HQGetOrderRes>() {
+					});
+			_LOG.info("**********timer task :" + resJson);
+			if (response.getResult().getStatus() && response.getOrder().getOrderStatus().equals("已取票")) {
+				// 修改订单明细的状态
+				OrderDetailExample updateExample = new OrderDetailExample();
+				updateExample.createCriteria()
+						.andIdEqualTo(orderDetail.getId())
+						.andVersionEqualTo(orderDetail.getVersion());
+				orderDetail.setStat(Constant.ORDER_STATUS_FINISH);
+				orderDetail.setVersion(orderDetail.getVersion() + 1);
+				orderDetail.setUpdateTime(new Date());
+				detailMapper.updateByExampleSelective(orderDetail, updateExample);
+
+				optlogService.saveLog(orderDetail.getOrderId(), Constant.ORDEROPT_TYPE_UPDATE, "【定时任务】同步环企订单状态【"
+								+ response.getOrder().getOrderStatus() + "】",
+						"TimerTask");
+				_LOG.info("**********【定时任务】同步环企订单状态【"
+						+ response.getOrder().getOrderStatus() + "】");
+			} else if (!response.getResult().getStatus()) {
+				optlogService.saveLog(orderDetail.getOrderId(),
+						Constant.ORDEROPT_TYPE_UPDATE, "【定时任务】同步环企订单状态失败【"
+								+ response.getResult().getErrormessage() + "】",
+						"TimerTask");
+				_LOG.info("**********【定时任务】同步环企订单状态失败【"
+						+ response.getResult().getErrormessage() + "】");
+			}
 		}
+
 	}
 
 }
--- task-utils/src/main/java/com/cuize/task/meta/Constant.java
+++ task-utils/src/main/java/com/cuize/task/meta/Constant.java
@@ -4,10 +4,22 @@ public interface Constant {
 	public static final Integer ORDER_STATUS_CREATE = 1;
 	public static final Integer ORDER_STATUS_PAID = 2;
 	public static final Integer ORDER_STATUS_FINISH = 3;
-	public static final Integer ORDER_STATUS_CANCEL = 7;
+	public static final Integer ORDER_STATUS_CANCEL = 4;
 	
 	public static final Integer ORDEROPT_TYPE_CREATE = 1;
 	public static final Integer ORDEROPT_TYPE_DELETE = 3;
 	public static final Integer ORDEROPT_TYPE_UPDATE = 2;
 	public static final Integer ORDEROPT_TYPE_QUERY = 4;
+	
+	public static final Integer THIRD_SYSTYPE_HIYO = 0;//嗨摇
+	public static final Integer THIRD_SYSTYPE_HUANQI = 1;//环企
+	public static final Integer THIRD_SYSTYPE_HUANQI_PAYFIRST = 2;//环企先支付
+	public static final Integer THIRD_SYSTYPE_SHENDA = 3;//深大
+	
+	public static final Integer TICKET_DAYTYPE_TODAY = 1;//当日票
+	public static final Integer TICKET_DAYTYPE_OTHERDAY = 2;//预约票
+	public static final Integer TICKET_DAYTYPE_NOTDISPLAY = 0;//不显示
+	public static final Integer TICKET_DAYTYPE_PACK = 3;//套票（打包票）
+	
+	public static final Integer PRODUCT_PACKTYPE_HUANQI = 1;//打包产品的打包类型
 }
