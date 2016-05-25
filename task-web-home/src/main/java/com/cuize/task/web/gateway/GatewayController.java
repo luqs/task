bc5f8f097c46dd823751a4b2a618719dc2d97d61
luqs <573843371@qq.com>
2016-05-25 11:26:28 +0800

    将共同component放进commons-util

--- task-web-home/src/main/java/com/cuize/task/web/gateway/GatewayController.java
+++ task-web-home/src/main/java/com/cuize/task/web/gateway/GatewayController.java
@@ -14,7 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.cuize.task.web.helper.JosnRPCBizHelper;
+import com.cuize.commons.meta.JosnRPCBizHelper;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Error;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
 
